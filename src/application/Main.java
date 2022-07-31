5c452944b32aafe4077012fd57f3126e63c0171b
Parsa Rohani Shahraki <parsa.rohanishahraki@ucalgary.ca>
2022-07-30 22:51:30 -0600

    Added label to my own branch.

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
 		try {
 			VBox root = new VBox();
 			root.getChildren().add(new Label ("Raisa's Label"));
+			root.getChildren().add(new Label("Parsa's Branch"));
 		
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
